5a490e5bf6d35e6915041fd1e0d94b3197760c57
RECNEPS-KIPS <2432719656@qq.com>
2020-04-05 16:37:19 +0800

    BuildPipeline.BuildAssetBundles的使用
    1.作用:用于打包资源
    2.参数,第一个为路径,第二个为压缩方式,第三个为资源包使用的目标平台
    3.压缩方式:<1>BuildAssetBundleOptions.None,使用LZMA方式压缩文件,体积小,解压速度慢,使用前整体解压,一旦解压会使用LZ4重新压缩,LZ4无需整体解压
    <2>BuildAssetBundleOptions.UncompressedAssetBundle,不压缩,包体积大,读取速度快
    <3>BuildAssetBundleOptions.ChunkBasedCompression,LZ4压缩,体积较LZMA大,速度快,可以加载指定资源,而非全部资源

--- Assets/Scripts/LoadAssetBundlesFromFile.cs
+++ Assets/Scripts/LoadAssetBundlesFromFile.cs
@@ -7,13 +7,14 @@ public class LoadAssetBundlesFromFile : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         string[] path = assetName.Split('/');
+        //获取路径
         string url = Application.streamingAssetsPath +"/"+ path[0] + "/" + path[1].ToLower() + ".assetbundle";
         Debug.Log(url);
+        //加载路径下的资源
         AssetBundle assetBundle = AssetBundle.LoadFromFile(url);
-        
         GameObject[] gos=assetBundle.LoadAllAssets<GameObject>();
         foreach (GameObject go in gos) {
-            Instantiate(go);
+            Instantiate(go);//实例化对象
         }
     }
 
