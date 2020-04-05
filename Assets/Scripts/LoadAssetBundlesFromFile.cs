62a48182d2fe3fc93f9b9e14ad111ff081d821c8
RECNEPS-KIPS <2432719656@qq.com>
2020-04-05 14:40:54 +0800

    实现了从本地AssetBundle包加载资源

---
+++ Assets/Scripts/LoadAssetBundlesFromFile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetBundlesFromFile : MonoBehaviour {
+    public string assetName;
+    // Start is called before the first frame update
+    void Start() {
+        string[] path = assetName.Split('/');
+        string url = Application.streamingAssetsPath +"/"+ path[0] + "/" + path[1].ToLower() + ".assetbundle";
+        Debug.Log(url);
+        AssetBundle assetBundle = AssetBundle.LoadFromFile(url);
+        
+        GameObject[] gos=assetBundle.LoadAllAssets<GameObject>();
+        foreach (GameObject go in gos) {
+            Instantiate(go);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
